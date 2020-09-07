6a7c3fce082efad72669fff69bb67275ceb150eb
Nouman Mahmood <noumanmhd@outlook.com>
2020-09-07 15:35:24 +0500

    Fix profile booking info

--- app/auth.py
+++ app/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash,
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required, current_user
 from .models import User
-from . import db, current_time, book_time
+from . import db, current_time, book_time, get_time
 from .models import Book
 
 auth = Blueprint('auth', __name__)
@@ -27,30 +27,47 @@ def book():
     else:
         empty = False
     return render_template('book.html', slots=slots, empty=empty, booked=booked)
+
+
 @auth.route('/profile')
 @login_required
 def profile():
     checks = Book.query.filter_by(plate=current_user.plate).all()
     booked = False
+    allocated = False
+    slot = ''
+    m = 0
+    s = 0
     if checks:
         for check in checks:
             if check.exptime > current_time():
                 booked = True
-    return render_template('profile.html', name=current_user.name, plate=current_user.plate, booked=booked)
+                slot = check.slot
+                m, s = get_time(str(check.exptime))
+                if int(check.status) != 0:
+                    allocated = True
+    return render_template('profile.html', name=current_user.name,
+            plate=current_user.plate, booked=booked,
+            allocated=allocated,
+            slot=slot, min=m, sec=s)
+
 
 @auth.route('/book', methods=['POST'])
 @login_required
 def book_post():
     slot = request.form.get('slot')
+    print(slot)
     book = Book.query.filter_by(slot=slot).first()
     book.plate = current_user.plate
     book.exptime = book_time()
     db.session.commit()
     return redirect(url_for('main.profile'))
 
+
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
 
 @auth.route('/login', methods=['POST'])
 def login_post():
@@ -68,9 +85,11 @@ def login_post():
 
     return redirect(url_for('main.profile'))
 
+
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
@@ -87,16 +106,18 @@ def signup_post():
         return redirect(url_for('auth.signup'))
 
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'),
-     plate=plate, subexp=subexp)
+                    plate=plate, subexp=subexp)
 
     db.session.add(new_user)
     db.session.commit()
 
     return redirect(url_for('auth.login'))
 
+
 @auth.route('/changepwd')
 def changepwd():
     return render_template('changepwd.html')
+
 
 @auth.route('/changepwd', methods=['POST'])
 def changepwd_post():
@@ -123,20 +144,22 @@ def changepwd_post():
 
     return redirect(url_for('main.profile'))
 
+
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
 
-@auth.route('/update_slot', methods=['GET','POST'])
+
+@auth.route('/update_slot', methods=['GET', 'POST'])
 def update_slot():
     if request.remote_addr == '127.0.0.1':
         data = request.json
-        for key,value in data.items():
+        for key, value in data.items():
             slot = Book.query.filter_by(slot=key).first()
             print("Key: {}, Value: {}".format(key, value))
-            slot.status = int(value) 
+            slot.status = int(value)
             print(slot.status)
         print(data)
         db.session.commit()
@@ -145,13 +168,14 @@ def update_slot():
     else:
         return Response("ERROR", status=403, mimetype='application/json')
 
-@auth.route('/get_plate', methods=['GET','POST'])
+
+@auth.route('/get_plate', methods=['GET', 'POST'])
 def get_plate():
     print(request.remote_addr)
     if request.remote_addr == '127.0.0.1':
         data = request.json
         print(data)
-        
+
         booked = False
         for r in data["results"]:
             checks = Book.query.filter_by(plate=r).all()
